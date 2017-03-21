eed9da8d9708570096e484cb356d0e045a78b42b
mbaye diop <mbaye.diop@suite.sn>
2017-03-21 03:48:06 +0000

    creation de quelques fonctions de base

--- gestion_membres_esp.c
+++ gestion_membres_esp.c
@@ -5,6 +5,51 @@
 char * cheminauth = "fichiers/authentification.txt";
 char * cheminmembres = "fichiers/membres.txt";
 
+typedef struct formation
+{
+	char * code;
+	char * intitule;
+	int annee;	
+} formation;
+
+typedef struct membre
+{
+	char * numero;
+	char * nom;
+	char * prenom;
+	char * adresse;
+	int nombreClasse;
+	formation * classe;
+	struct membre * suiv;
+} * membre;
+
+ //cette fonction nous permet de creer une liste vide des membres
+Membres creerListeMembreLC(){
+	membrs l = (membre)malloc(sizeof(struct membre));
+	l = NULL;
+	return l;
+}
+
+//insertion d'une classe de formation
+void inserrerClasseLC(struct membre m)
+
+//inserer un membre dans la liste
+liste insererMembreLC(membre l, struct membre m)
+{
+	membre tete = l;
+	while(tete->suiv != NULL)
+		tete = tete->suiv;
+	tete->suiv = m;
+        m->suiv = NULL;
+	return l;
+}
+
+Membre ajouterMembreLC(Membres l,char * numero)
+{
+
+}
+
+
 
 int main(int argc, char const *argv[])
 {
